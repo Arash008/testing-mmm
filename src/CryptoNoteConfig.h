143ce63e823f444d78b2ba1c0c932f029b59e073
Arash008 <33556597+Arash008@users.noreply.github.com>
2017-12-02 03:16:48 +0330

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "firstmoney";
-char GENESIS_COINBASE_TX_HEX[]                               = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101af7e537fc4d8281f0f97f8de913bace5e44127b2cc7d9a8bec258b352fcd7a6f";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001ffffffffffff0f029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101af7e537fc4d8281f0f97f8de913bace5e44127b2cc7d9a8bec258b352fcd7a6f";
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
 const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
